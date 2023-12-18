d3783ff87cdc5d389dd36b511f34a5d25b1ef960
aleksyprok <aleksyprok@gmail.com>
2023-12-18 20:35:44 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,50 +1,49 @@
 import yfinance as yf
 import pandas as pd
-import numpy as np
 
-# Fetch historical data for MSFT and AAPL
-msft = yf.download('MSFT', start='2020-01-01', end='2023-01-01')['Close']
-aapl = yf.download('AAPL', start='2020-01-01', end='2023-01-01')['Close']
+def fetch_data(symbol, start_date, end_date):
+    return yf.download(symbol, start=start_date, end=end_date)['Close']
 
-# Calculate the ratio of the two stocks
-ratio = msft / aapl
+def calculate_ratio(stock_a, stock_b):
+    return stock_a / stock_b
 
-# Define thresholds for buying and selling
-mean = ratio.mean()
-std = ratio.std()
-sell_threshold = mean + 0.5 * std
-buy_threshold = mean - 0.5 * std
+def execute_strategy(ratio):
+    mean = ratio.mean()
+    std = ratio.std()
+    sell_threshold = mean + 0.5 * std
+    buy_threshold = mean - 0.5 * std
 
-# Initialize trade log and pnl
-trade_log = pd.DataFrame(index=ratio.index, columns=['Position', 'PnL'])
-pnl = 0
-position = None  # 'long MSFT', 'long AAPL', or None
+    trade_log = pd.DataFrame(index=ratio.index, columns=['Position', 'PnL'])
+    pnl = 0
+    position = None  # 'long MSFT', 'long AAPL', or None
 
-for i in range(1, len(ratio)):
-    if position is None:
-        if ratio.iloc[i] > sell_threshold:
-            position = 'long AAPL'  # Buy AAPL, Sell MSFT
-            entry_price_msft = msft.iloc[i]
-            entry_price_aapl = aapl.iloc[i]
-        elif ratio.iloc[i] < buy_threshold:
-            position = 'long MSFT'  # Buy MSFT, Sell AAPL
-            entry_price_msft = msft.iloc[i]
-            entry_price_aapl = aapl.iloc[i]
+    for i in range(1, len(ratio)):
+        if position is None:
+            if ratio.iloc[i] > sell_threshold:
+                position = 'long AAPL'
+                entry_price_msft = msft.iloc[i]
+                entry_price_aapl = aapl.iloc[i]
+            elif ratio.iloc[i] < buy_threshold:
+                position = 'long MSFT'
+                entry_price_msft = msft.iloc[i]
+                entry_price_aapl = aapl.iloc[i]
+        elif position == 'long MSFT' and ratio.iloc[i] <= mean:
+            pnl += ((msft.iloc[i] - entry_price_msft) / entry_price_msft - 
+                    (aapl.iloc[i] - entry_price_aapl) / entry_price_aapl) * 100
+            position = None
+        elif position == 'long AAPL' and ratio.iloc[i] >= mean:
+            pnl += ((aapl.iloc[i] - entry_price_aapl) / entry_price_aapl - 
+                    (msft.iloc[i] - entry_price_msft) / entry_price_msft) * 100
+            position = None
 
-    elif position == 'long MSFT' and ratio.iloc[i] <= mean:
-        # Close position
-        pnl += ((msft.iloc[i] - entry_price_msft) / entry_price_msft - 
-                (aapl.iloc[i] - entry_price_aapl) / entry_price_aapl) * 100
-        position = None
+        trade_log.at[trade_log.index[i], 'Position'] = position
+        trade_log.at[trade_log.index[i], 'PnL'] = pnl
 
-    elif position == 'long AAPL' and ratio.iloc[i] >= mean:
-        # Close position
-        pnl += ((aapl.iloc[i] - entry_price_aapl) / entry_price_aapl - 
-                (msft.iloc[i] - entry_price_msft) / entry_price_msft) * 100
-        position = None
+    return pnl, trade_log
 
-    trade_log.at[trade_log.index[i], 'Position'] = position
-    trade_log.at[trade_log.index[i], 'PnL'] = pnl
-
-# Display final PnL
+# Example usage
+msft = fetch_data('MSFT', '2020-01-01', '2023-01-01')
+aapl = fetch_data('AAPL', '2020-01-01', '2023-01-01')
+ratio = calculate_ratio(msft, aapl)
+pnl, trade_log = execute_strategy(ratio)
 print(f"Total PnL: {pnl}%")

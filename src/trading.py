602f9f71c8eceff5571bfb12c53e921aecfaa5e3
aleksyprok <aleksyprok@gmail.com>
2023-12-19 21:01:27 +0000

    Update trading.py

--- src/trading.py
+++ src/trading.py
@@ -5,7 +5,8 @@ Functions to execute pairs trading strategy.
 import yfinance as yf
 
 def run_pairs_trade_strategy(stock_a_ticker: str = 'AAPL',
-                             stock_b_ticker: str = 'MSFT'):
+                             stock_b_ticker: str = 'MSFT',
+                             training_start_date: str = ''):
     print(stock_a_ticker)
     print(stock_b_ticker)
     return 1

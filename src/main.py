a2fb943bf8ac28997f986598d7173e3092f1f40d
aleksyprok <aleksyprok@gmail.com>
2023-12-19 23:31:30 +0000

    update .gitignore

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # import yfinance as yf
 # import pandas as pd
-import trading
+from .trading import run_pairs_trade_strategy
 
 # def fetch_data(symbol, start_date, end_date):
 #     return yf.download(symbol, start=start_date, end=end_date)['Close']
@@ -55,4 +55,4 @@ import trading
 # ratio = calculate_ratio(msft, aapl)
 # pnl, trade_log = execute_strategy(ratio)
 # print(f"Total PnL: {pnl}%")
-trading.run_pairs_trade_strategy()
+run_pairs_trade_strategy()

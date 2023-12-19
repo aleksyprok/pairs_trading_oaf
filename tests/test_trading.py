2829cd3941930843070403d965bcf710d8a58745
aleksyprok <aleksyprok@gmail.com>
2023-12-19 23:53:35 +0000

    Update test_trading.py

--- tests/test_trading.py
+++ tests/test_trading.py
@@ -15,7 +15,6 @@ def test_run_pairs_trade_strategy_custom_tickers():
     """
     Test the run_pairs_trade_strategy function with custom ticker parameters.
     """
-    
     custom_ticker_a = "GOOGL"
     custom_ticker_b = "TSLA"
     result = trading.run_pairs_trade_strategy(stock_a_ticker=custom_ticker_a,

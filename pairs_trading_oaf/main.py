649f0dd03f033227925be7de442585db62a0ee5c
aleksyprok <aleksyprok@gmail.com>
2023-12-20 13:01:17 +0000

    Update trading.py

--- pairs_trading_oaf/main.py
+++ pairs_trading_oaf/main.py
@@ -1,59 +1,6 @@
-# import yfinance as yf
-# import pandas as pd
+"""
+Main module for pairs trading strategy.
+"""
 import pairs_trading_oaf.trading as trading
 
-# def fetch_data(symbol, start_date, end_date):
-#     return yf.download(symbol, start=start_date, end=end_date)['Close']
-
-# def calculate_ratio(stock_a, stock_b):
-#     return stock_a / stock_b
-
-# def execute_strategy(ratio):
-#     mean = ratio.mean()
-#     std = ratio.std()
-#     sell_threshold = mean + 0.5 * std
-#     buy_threshold = mean - 0.5 * std
-
-#     trade_log = pd.DataFrame(index=ratio.index, columns=['Position', 'PnL'])
-#     pnl = 0
-#     position = None  # 'long MSFT', 'long AAPL', or None
-
-#     for i in range(1, len(ratio)):
-#         if position is None:
-#             if ratio.iloc[i] > sell_threshold:
-#                 position = 'long AAPL'
-#                 entry_price_msft = msft.iloc[i]
-#                 entry_price_aapl = aapl.iloc[i]
-#             elif ratio.iloc[i] < buy_threshold:
-#                 position = 'long MSFT'
-#                 entry_price_msft = msft.iloc[i]
-#                 entry_price_aapl = aapl.iloc[i]
-#         elif position == 'long MSFT' and ratio.iloc[i] <= mean:
-#             pnl += ((msft.iloc[i] - entry_price_msft) / entry_price_msft -
-#                     (aapl.iloc[i] - entry_price_aapl) / entry_price_aapl) * 100
-#             position = None
-#         elif position == 'long AAPL' and ratio.iloc[i] >= mean:
-#             pnl += ((aapl.iloc[i] - entry_price_aapl) / entry_price_aapl -
-#                     (msft.iloc[i] - entry_price_msft) / entry_price_msft) * 100
-#             position = None
-
-#         trade_log.at[trade_log.index[i], 'Position'] = position
-#         trade_log.at[trade_log.index[i], 'PnL'] = pnl
-
-#     return pnl, trade_log
-
-# Calculate ratio
-# If ratio > threshold:
-# Short A and long B
-# If ratio < threshhold:
-# Long A and short B
-# Possible positions = [None, "long A short B", "short A long B"
-
-# # Example usage
-# msft = fetch_data('MSFT', '2020-01-01', '2023-01-01')
-# aapl = fetch_data('AAPL', '2020-01-01', '2023-01-01')
-# ratio = calculate_ratio(msft, aapl)
-# pnl, trade_log = execute_strategy(ratio)
-# print(f"Total PnL: {pnl}%")
-print(2)
 trading.run_pairs_trade_strategy()
--- pairs_trading_oaf/trading.py
+++ pairs_trading_oaf/trading.py
@@ -9,19 +9,99 @@ class Portfolio:
     Class to represent a portfolio.
     """
 
-    def __init__(self):
+    def __init__(self, stock_a_ticker: str, stock_b_ticker: str):
         """
         Initialize the portfolio.
         """
         self.cash = 0
         self.stock_a = 0
         self.stock_b = 0
+        self.position = None
+        self.position_limit = 1000000 # USD
+        self.stock_a_ticker = stock_a_ticker
+        self.stock_b_ticker = stock_b_ticker
+        self.long_stock_a_threshold = None
+        self.long_stock_b_threshold = None
+        self.cash_over_time = []
+        self.stock_a_over_time = []
+        self.stock_b_over_time = []
+        self.position_over_time = []
 
     def get_value(self, stock_a_price, stock_b_price):
         """
         Get the value of the portfolio.
         """
         return self.cash + self.stock_a * stock_a_price + self.stock_b * stock_b_price
+
+    def calculate_trading_thresholds(self, training_start_date: str, training_end_date: str):
+        """
+        Calculate and store the trading thresholds to the portfolio.
+        """
+        stock_a_training_prices = yf.download(self.stock_a_ticker,
+                                              start=training_start_date,
+                                              end=training_end_date)['Close']
+        stock_b_training_prices = yf.download(self.stock_b_ticker,
+                                              start=training_start_date,
+                                              end=training_end_date)['Close']
+        ratio = stock_a_training_prices / stock_b_training_prices
+
+        mean = ratio.mean()
+        std = ratio.std()
+
+        long_stock_a_threshold = mean - 0.5 * std
+        long_stock_b_threshold = mean + 0.5 * std
+
+        self.long_stock_a_threshold = long_stock_a_threshold
+        self.long_stock_b_threshold = long_stock_b_threshold
+
+    def execute_trades(self, start_date: str, end_date: str):
+        """
+        Execute trades based on the trading thresholds.
+        """
+        stock_a_prices = yf.download(self.stock_a_ticker,
+                                     start=start_date,
+                                     end=end_date)['Close']
+        stock_b_prices = yf.download(self.stock_b_ticker,
+                                     start=start_date,
+                                     end=end_date)['Close']
+        ratio = stock_a_prices / stock_b_prices
+
+        for it in range(len(ratio)):
+            if self.position is None:
+                if ratio.iloc[it] < self.long_stock_a_threshold:
+                    self.position = 'long A short B'
+                    self.stock_a = +self.position_limit / stock_a_prices.iloc[it]
+                    self.stock_b = -self.position_limit / stock_b_prices.iloc[it]
+                elif ratio.iloc[it] > self.long_stock_b_threshold:
+                    self.position = 'short A long B'
+                    self.stock_a = -self.position_limit / stock_a_prices.iloc[it]
+                    self.stock_b = +self.position_limit / stock_b_prices.iloc[it]
+            elif self.position == 'short A long B' and ratio.iloc[it] < self.long_stock_a_threshold:
+                # Flip postion from short A long B to long A short B
+                # Sell B and buy A
+                self.cash -= self.stock_a * stock_a_prices.iloc[it]
+                self.cash += self.stock_b * stock_b_prices.iloc[it]
+                self.stock_a = +self.position_limit / stock_a_prices.iloc[it]
+                self.stock_b = -self.position_limit / stock_b_prices.iloc[it]
+                self.position = 'long A short B'
+            elif self.position == 'long A short B' and ratio.iloc[it] > self.long_stock_b_threshold:
+                # Flip postion from long A short B to short A long B
+                # Sell A and buy B
+                self.cash += self.stock_a * stock_a_prices.iloc[it]
+                self.cash -= self.stock_b * stock_b_prices.iloc[it]
+                self.stock_a = -self.position_limit / stock_a_prices.iloc[it]
+                self.stock_b = +self.position_limit / stock_b_prices.iloc[it]
+                self.position = 'short A long B'
+            self.update_over_time_values()
+
+    def update_over_time_values(self):
+        """
+        Update the portfolio over time.
+        """
+        self.cash_over_time.append(self.cash)
+        self.stock_a_over_time.append(self.stock_a)
+        self.stock_b_over_time.append(self.stock_b)
+        self.position_over_time.append(self.position)
 
 def run_pairs_trade_strategy(stock_a_ticker: str = 'AAPL',
                              stock_b_ticker: str = 'MSFT',
@@ -33,52 +113,8 @@ def run_pairs_trade_strategy(stock_a_ticker: str = 'AAPL',
     Run the pairs trading strategy.
     """
 
-    stock_a_training_prices = yf.download(stock_a_ticker,
-                                          start=training_start_date,
-                                          end=training_end_date)['Close']
-    stock_b_training_prices = yf.download(stock_b_ticker,
-                                          start=training_start_date,
-                                          end=training_end_date)['Close']
-    stock_a_testing_prices = yf.download(stock_a_ticker,
-                                         start=testing_start_date,
-                                         end=testing_end_date)['Close']
-    stock_b_testing_prices = yf.download(stock_b_ticker,
-                                         start=testing_start_date,
-                                         end=testing_end_date)['Close']
-    
-    ratio = stock_a_training_prices / stock_b_training_prices
+    portfolio = Portfolio(stock_a_ticker, stock_b_ticker)
+    portfolio.calculate_trading_thresholds(training_start_date, training_end_date)
+    portfolio.execute_trades(testing_start_date, testing_end_date)
 
-    mean = ratio.mean()
-    std = ratio.std()
-
-    long_stock_a_threshold = mean - 0.5 * std
-    long_stock_b_threshold = mean + 0.5 * std
-
-    # Execute strategy
-    execute_trades(stock_a_ticker,
-                   stock_b_ticker,
-                   testing_start_date,
-                   testing_end_date,
-                   long_stock_a_threshold,
-                   long_stock_b_threshold)
-    
     return 1
-
-def execute_trades(stock_a_ticker, stock_b_ticker, start_date, end_date,
-                   long_stock_a_threshold, long_stock_b_threshold):
-
-    stock_a_prices = yf.download(stock_a_ticker,
-                                 start=start_date,
-                                 end=end_date)['Close']
-    stock_b_prices = yf.download(stock_b_ticker,
-                                 start=start_date,
-                                 end=end_date)['Close']
-    ratio = stock_a_prices / stock_b_prices
-
-    for it in range(len(ratio)):
-        if ratio.iloc[it] < long_stock_a_threshold:
-            print(f"Long {stock_a_ticker} and short {stock_b_ticker}")
-        elif ratio.iloc[it] > long_stock_b_threshold:
-            print(f"Short {stock_a_ticker} and long {stock_b_ticker}")
-        else:
-            print("No trade")

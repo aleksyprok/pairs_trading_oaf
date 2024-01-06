a555c274123b70d65b34ed92e1a69e4c576f4eab
aleksyprok <aleksyprok@gmail.com>
2024-01-06 00:46:49 +0000

    Added more pairs

--- pairs_trading_oaf/main.py
+++ pairs_trading_oaf/main.py
@@ -12,15 +12,27 @@ When we long stock A and short stock B, we buy position_limit worth of stock A a
 position_limit worth of stock B and vice versa when we long stock B and short stock A.
 """
 
+import time
 import matplotlib.pyplot as plt
 from pairs_trading_oaf import trading, portfolio, strategies, plotting
+
+tic = time.perf_counter()
 
 TRAINING_DATA_FNAME = "Price Data - CSV - Formation Period.csv"
 TESTING_DATA_FNAME = "Price Data - CSV - Trading Period.csv"
 POSITION_LIMIT = int(1e0)
+
 stock_pair_labels_list = [
     ("Microsoft Corporation (NasdaqGS:MSFT)", "Apple Inc. (NasdaqGS:AAPL)"),
     ("Bank of America Corporation (NYSE:BAC)", "JPMorgan Chase & Co. (NYSE:JPM)"),
+    ("Exxon Mobil Corporation (NYSE:XOM)", "Chevron Corporation (NYSE:CVX)"),
+    ("Walmart Inc. (NYSE:WMT)", "Target Corporation (NYSE:TGT)"),
+    ("The Coca-Cola Company (NYSE:KO)", "PepsiCo, Inc. (NasdaqGS:PEP)"),
+    ("Johnson & Johnson (NYSE:JNJ)", "Abbott Laboratories (NYSE:ABT)"),
+    ("The Procter & Gamble Company (NYSE:PG)", "Unilever PLC (LSE:ULVR)"),
+    ("The Boeing Company (NYSE:BA)", "Airbus SE (ENXTPA:AIR)"),
+    ("Caterpillar Inc. (NYSE:CAT)", "Deere & Company (NYSE:DE)"),
+    ("Wells Fargo & Company (NYSE:WFC)", "Citigroup Inc. (NYSE:C)"),
 ]
 StrategyClass = strategies.StrategyA
 
@@ -37,5 +49,8 @@ for stock_pair_labels in stock_pair_labels_list:
 trading.simulate_trading(master_portfolio)
 
 plotting.plot_portfolio_value_over_time(master_portfolio)
+
+toc = time.perf_counter()
+print(f"Time taken: {toc - tic:0.4f} seconds")
 
 plt.show()

52235648c2afad5ec088e020c40c2f06df5173d3
aleksyprok <aleksyprok@gmail.com>
2024-02-10 20:22:36 +0000

    Added crypto results

--- data/create_ETF_csv.py
+++ data/create_ETF_csv.py
@@ -7,7 +7,6 @@ The data is then saved to CSV files for further use in the analysis.
 
 import yfinance as yf
 
-# Define the ETFs and their formatted names
 etfs = {
     'SPY': 'SPDR S&P 500 ETF Trust (NYSE Arca:SPY)',
     'IVV': 'iShares Core S&P 500 ETF (NYSE Arca:IVV)',
@@ -21,42 +20,27 @@ etfs = {
     'SLV': 'iShares Silver Trust (NYSE Arca:SLV)'
 }
 
-# Initial period
+# Formation period
 START_DATE_INITIAL = '2010-01-04'
 END_DATE_INITIAL = '2020-12-31'
-
-# Fetching the initial period data
 data_initial = yf.download(list(etfs.keys()),
                            start=START_DATE_INITIAL,
                            end=END_DATE_INITIAL)['Adj Close']
-
-# Reformatting column names
 data_initial.rename(columns=etfs, inplace=True)
-
-# Reformatting the data to match the desired format
 data_initial.reset_index(inplace=True)
 data_initial.rename(columns={'Date': 'Closing Date'}, inplace=True)
-
-# Save the initial period data to a CSV file
 CSV_FILE_PATH_INITIAL = 'Price Data - CSV - Formation Period - ETF.csv'
 data_initial.to_csv(CSV_FILE_PATH_INITIAL, index=False)
 
 # Trading period
 START_DATE_TRADING = '2021-01-04'
 END_DATE_TRADING = '2023-06-30'
-
-# Fetching the trading period data
 data_trading = yf.download(list(etfs.keys()),
                            start=START_DATE_TRADING,
                            end=END_DATE_TRADING)['Adj Close']
 
-# Reformatting column names
 data_trading.rename(columns=etfs, inplace=True)
-
-# Reformatting the data to match the desired format
 data_trading.reset_index(inplace=True)
 data_trading.rename(columns={'Date': 'Closing Date'}, inplace=True)
-
-# Save the trading period data to a CSV file
 CSV_FILE_PATH_TRADING = 'Price Data - CSV - Trading Period - ETF.csv'
 data_trading.to_csv(CSV_FILE_PATH_TRADING, index=False)
